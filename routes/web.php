1f761228d2994439bce4118592b9a5e7acabe8b1
Ivan Javorovic <iwan.javorovic@gmail.com>
2018-11-03 20:52:40 +0100

    email-verification

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['prefix' => '/', 'namespace' => 'Frontend', 'as' => 'frontend.'], 
 
 
 // ROUTES ONLY FOR REGISTERED USERS WHICH HAVE FILLED BASE INFO
-Route::group(['prefix' => '/', 'namespace' => 'Frontend', 'as' => 'frontend.', 'middleware' => ['auth','steps']], function () {
+Route::group(['prefix' => '/', 'namespace' => 'Frontend', 'as' => 'frontend.', 'middleware' => ['verified','auth','steps']], function () {
 
     //HOME
     Route::get('/', 'HomeController@index')->name('home');
